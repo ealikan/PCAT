a250464b753a8f562a4bcbc89a0e1b45da965709
ealikan <ethem_alikan@hotmail.com>
2023-08-28 13:19:43 +0300

    güncellemeler yapıldı

--- app.js
+++ app.js
@@ -1,23 +1,41 @@
 const express = require("express");
 const ejs = require("ejs");
 const path = require("path");
+const mongoose = require("mongoose");
+const Photo = require("./models/Photo");
+
 const app = express();
+
+//connect DB
+mongoose.connect("mongodb://127.0.0.1:27017/pcat-test-db", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 //Template Engine
 app.set("view engine", "ejs");
 
 //middlewares
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //Routes
-app.get("/", (req, res) => {
-  res.render("index");
+app.get("/", async (req, res) => {
+  const photos = await Photo.find({});
+  res.render("index", {
+    photos,
+  });
 });
 app.get("/about", (req, res) => {
   res.render("about");
 });
 app.get("/add", (req, res) => {
   res.render("add");
+});
+app.post("/photos", async (req, res) => {
+  await Photo.create(req.body);
+  res.redirect("/");
 });
 
 const port = 3000;
